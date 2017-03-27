c2b2b5353ab27f368467787217c4e20ff99ea5e2
NguyenTuanSieu <dev.phpjs@gmail.com>
2017-03-28 01:48:59 +0700

    .

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -25,53 +25,53 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'My Company',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
+<section class="content">
+    <header class="top-head container-fluid">
+        <button type="button" class="navbar-toggle pull-left">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <nav class=" navbar-default" role="navigation">
+            <ul class="nav navbar-nav navbar-right top-menu top-right-menu">
+                <li class="dropdown text-center">
+                    <?php if (!Yii::$app->user->isGuest): ?>
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                        <img alt="" src="img/avatar-2.jpg" class="img-circle profile-img thumb-sm">
+                        <span class="username"><?= Yii::$app->user->identity->username; ?> </span> <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu pro-menu fadeInUp animated" tabindex="5003" style="overflow: hidden; outline: none;">
+                        <li><a href="profile.html"><i class="fa fa-briefcase"></i>Trang cá nhân</a></li>
+                        <li><a href="#"><i class="fa fa-cog"></i> Đổi mật khẩu</a></li>
+                        <li>
+                            <?=Html::beginForm(['/site/logout'], 'post'); ?>
+                            <?=Html::submitButton('<i class="fa fa-sign-out"></i> Thoát'); ?>
+                            <?=Html::endForm(); ?>
+                        </li>
+                    </ul>
+                    <?php endif; ?>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="wraper container-fluid">
+        <div class="page-title">
+            <h3 class="title">Blank Page</h3>
+        </div>
         <?= Alert::widget() ?>
-        <?= $content ?>
+        <div class="row">
+            <div class="panel panel-purple">
+                <div class="panel-body">
+                    <?= $content ?>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+    <footer class="footer">
+        2017@VinPearl CMS
+    </footer>
+</section>
 
 <?php $this->endBody() ?>
 </body>
