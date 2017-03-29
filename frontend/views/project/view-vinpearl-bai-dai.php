977bd2ed4216c15c6ed1fd803b1b78aaa83a2ec9
NguyenTuanSieu <dev.phpjs@gmail.com>
2017-03-29 11:18:56 +0700

    .

--- frontend/views/project/view-vinpearl-bai-dai.php
+++ frontend/views/project/view-vinpearl-bai-dai.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+use yii\web\View;
 
 $this->title = $model->title . ' | Biệt thự biển VinPearl';
 $this->registerJsFile(
@@ -236,7 +237,65 @@ $this->registerCssFile('@web/css/baidai.css', [
         </div>
     </div>
 </div>
-
+<!--Hoi dap-->
+<div class="section-hoidap">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <?php
+                $video = (new \yii\db\Query())
+                    ->select(['video_link'])
+                    ->from('video')
+                    ->where(['project_id' => 1])
+                    ->one();
+                ?>
+                <div class="videoWrapper">
+                    <iframe width="585" height="329"
+                            src="<?php echo str_replace('watch?v=', 'embed/', $video['video_link']) ?>" frameborder="0"
+                            allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <?php
+                $testimonials = (new \yii\db\Query())
+                    ->select(['content', 'full_name', 'company'])
+                    ->from('testimonial')
+                    ->limit(3)
+                    ->all();
+                ?>
+                <div class="testimonial">
+                    <h3 class="testimonial--title">Câu chuyện khách hàng</h3>
+                    <div class="testimonial--content">
+                        <ul class="rslides1">
+                            <?php foreach ($testimonials as $testimonial): ?>
+                                <li>
+                                    <div class="testimonial--content-text">
+                                        <?= \yii\helpers\StringHelper::truncateWords($testimonial['content'], 120) ?>
+                                        <p>&nbsp;</p>
+                                        <p><strong><?= $testimonial['full_name'] ?></strong></p>
+                                        <p><i><?= $testimonial['company'] ?> đã chia sẻ</i></p>
+                                    </div>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </div>
+                <?php
+                $js = <<<JS
+   $(function() {
+    $(".rslides1").responsiveSlides({
+        speed: 500,
+        timeout: 3000,
+        pause: true
+    });
+  });
+JS;
+                $this->registerJs($js, View::POS_END);
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
 <!--Ly do chon-->
 <div class="section-lydo">
     <div class="container">
@@ -246,19 +305,20 @@ $this->registerCssFile('@web/css/baidai.css', [
                     <?= Html::img('/img/logo-white.png', ['class' => 'img-responsive text-center center-block']) ?>
                 </div>
             </div>
+            <hr class="hr-blank"/>
             <div class="col-xs-12">
                 <div class="center-block text-center">
                     <p class="section-lydo--title">Lý do chọn đầu tư</p>
                     <p class="section-lydo--title">VinPearl Bãi Dài</p>
                 </div>
             </div>
+            <hr class="hr-blank"/>
         </div>
         <div class="section-lydo--desc">
         <div class="row">
             <div class="col-xs-12 col-md-6">
                 <div class="pull-right">
                     <p class="section-lydo--desc-title">Sở hữu Vĩnh viễn</p>
-                    <hr class="hr-blank"/>
                     <p class="section-lydo--desc-title">Sinh lời Trọn đời</p>
 
                     <ul>
